34766cceca29b9d8092fdc168441a109f8bb7f41
zhangxiyanfcb <1277916877@qq.com>
2019-05-10 13:23:48 +0800

    提交成功11

---
+++ ly-getaway/src/main/java/com/leyou/LyGateway.java
@@ -0,0 +1,17 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author zhangxiyan
+ * @create 2019-04-25 14:49
+ */
+@SpringCloudApplication
+@EnableZuulProxy
+public class LyGateway {
+    public static void main(String[] args) {
+        SpringApplication.run(LyGateway.class, args);
+    }
+}
